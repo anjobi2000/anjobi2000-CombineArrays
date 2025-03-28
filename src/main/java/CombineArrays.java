4ef3416ce744efd32c72212ccfaa23571ba8fca5
Angela Jobi <Angela.chemmazham@gmail.com>
2025-03-28 14:56:57 +0000

    Completed Coding Challenge Combine Arrays

--- src/main/java/CombineArrays.java
+++ src/main/java/CombineArrays.java
@@ -8,7 +8,12 @@ public class CombineArrays {
      * @return an array containing the contents of arr1 followed by the contents of arr2.
      */
     public int[] combine(int[] arr1, int[] arr2){
+        if (arr1 == null) arr1 = new int[0];
+        if (arr2 == null) arr2 = new int[0];
+        int[] result = new int[arr1.length + arr2.length];
+        System.arraycopy(arr1, 0, result, 0, arr1.length);
+        System.arraycopy(arr2, 0, result, arr1.length, arr2.length);
 
-        return null;
+        return result;
     }
 }
